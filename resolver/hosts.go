978b18d32e67920d66579f30ee31b85a3c6806b0
Guofan Xu <xuguofan@live.com>
2022-02-16 14:58:38 +0800

    read system hosts
    Signed-off-by: Guofan Xu <xuguofan@live.com>

--- resolver/hosts.go
+++ resolver/hosts.go
@@ -3,13 +3,47 @@ package resolver
 import (
 	"fmt"
 	"github.com/miekg/dns"
+	"io/ioutil"
+	"log"
 	"net"
+	"os"
+	"regexp"
+	"runtime"
+	"strings"
 )
 
 type Hosts map[string]string
 
+func (h Hosts) loadSystemHosts() {
+	var hostLocation string
+	if runtime.GOOS == "windows" {
+		hostLocation = "%SystemRoot%\\System32\\drivers\\etc\\hosts"
+	} else {
+		hostLocation = "/etc/hosts"
+	}
+	open, err := os.Open(hostLocation)
+	if err != nil {
+		log.Println("read hosts file error:", err)
+	}
+	defer open.Close()
+	all, _ := ioutil.ReadAll(open)
+	lines := strings.Split(string(all), "\n")
+	re := regexp.MustCompile("\\s+")
+	for _, line := range lines {
+		if !strings.HasPrefix(line, "#") && strings.TrimSpace(line) != "" {
+			split := re.Split(line, -1)
+			if len(split) == 2 {
+				h[dns.Fqdn(split[1])] = split[0]
+			} else if len(split) != 0 {
+				log.Printf("Invalid line in hosts: %s", line)
+			}
+		}
+	}
+}
+
 func NewHosts(m map[string]string) Hosts {
 	hosts := make(Hosts, 0)
+	hosts.loadSystemHosts()
 	for k, v := range m {
 		if k != "" && v != "" {
 			hosts[dns.Fqdn(k)] = v
